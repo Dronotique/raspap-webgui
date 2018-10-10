fb27fa80fa172c21fcf4e688043c6ec71cb7bf7e
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-10-10 12:27:55 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,14 +4,17 @@ define('RASPI_VERSION', '1.3.1');
 define('RASPI_CONFIG', '/etc/raspap');
 define('RASPI_CONFIG_NETWORKING',RASPI_CONFIG.'/networking');
 define('RASPI_ADMIN_DETAILS', RASPI_CONFIG.'/raspap.auth');
-define('RASPI_WIFI_CLIENT_INTERFACE', 'wlan0');
+#define('RASPI_WIFI_CLIENT_INTERFACE', 'wlan0');
+define('RASPI_WIFI_CLIENT_INTERFACE', 'wlan0_client');
+
 
 // Constants for configuration file paths.
 // These are typical for default RPi installs. Modify if needed.
 define('RASPI_DNSMASQ_CONFIG', '/etc/dnsmasq.conf');
 define('RASPI_DNSMASQ_LEASES', '/var/lib/misc/dnsmasq.leases');
 define('RASPI_HOSTAPD_CONFIG', '/etc/hostapd/hostapd.conf');
-define('RASPI_WPA_SUPPLICANT_CONFIG', '/etc/wpa_supplicant/wpa_supplicant.conf');
+#define('RASPI_WPA_SUPPLICANT_CONFIG', '/etc/wpa_supplicant/wpa_supplicant.conf');
+define('RASPI_WPA_SUPPLICANT_CONFIG', '/etc/wpa_supplicant/wpa_supplicant-wlan0_client.conf');
 define('RASPI_HOSTAPD_CTRL_INTERFACE', '/var/run/hostapd');
 define('RASPI_WPA_CTRL_INTERFACE', '/var/run/wpa_supplicant');
 define('RASPI_OPENVPN_CLIENT_CONFIG', '/etc/openvpn/client.conf');
@@ -31,4 +34,4 @@ define('RASPI_VNSTAT_ENABLED', true );
 // Locale settings
 define('LOCALE_ROOT', 'locale');
 define('LOCALE_DOMAIN', 'messages'); 
-
+define('RASPI_WIFI_COUNTRY_CODE','FR');
