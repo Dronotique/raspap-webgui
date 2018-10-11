1414081430322995d769ab493d56c28769c9c096
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-10-11 14:26:31 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -11,7 +11,7 @@ define('RASPI_WIFI_CLIENT_INTERFACE', 'wlan1');
 // These are typical for default RPi installs. Modify if needed.
 define('RASPI_DNSMASQ_CONFIG', '/etc/dnsmasq.conf');
 define('RASPI_DNSMASQ_LEASES', '/var/lib/misc/dnsmasq.leases');
-define('RASPI_HOSTAPD_CONFIG', '/etc/hostapd/hostapd.conf');
+define('RASPI_HOSTAPD_CONFIG', '/etc/hostapd.conf');
 define('RASPI_WPA_SUPPLICANT_CONFIG', '/etc/wpa_supplicant/wpa_supplicant.conf');
 define('RASPI_HOSTAPD_CTRL_INTERFACE', '/var/run/hostapd');
 define('RASPI_WPA_CTRL_INTERFACE', '/var/run/wpa_supplicant');
