fe9803c70bdd6d85f39eae38264d6e75925c7b01
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-10-16 00:25:47 +0200

    test

--- includes/configurator.php
+++ includes/configurator.php
@@ -1,6 +1,6 @@
 <?php 
 function DisplayConnexionConfig() {
-    $xmlConfFile = simplexml_load_file("ffconfiguration.xml");
+    $xmlConfFile = simplexml_load_file("configuration.xml");
     $availableCnx = $xmlConfFile->children('/connexions');
     
     echo ($xmlConfFile);
