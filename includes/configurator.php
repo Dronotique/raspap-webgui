6f77493b28e99b636cf022cfd840481be3732c58
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-10-15 22:17:23 +0200

    confoig

--- includes/configurator.php
+++ includes/configurator.php
@@ -4,40 +4,75 @@ function DisplayConnexionConfig() {
 <div class="row">
     <div class="col-lg-12">
       <div class="panel panel-primary">
-        <div class="panel-heading"><i class="fa fa-lock fa-fw"></i><?php echo _("Configure Auth"); ?></div>
+        <div class="panel-heading"><i class="fa fa-lock fa-fw"></i><?php echo _("Configure COnnexions"); ?></div>
         <div class="panel-body" id="xonomy_editor">
       		<form method="POST" action="?page=configurator" name="conf_form">
-      			<div id="connexion1">
-      				Serial port [USB-SERIAL CH340 (COM5)] 57600 <input type="submit" name="delete_1" label="Delete"/>
-      			</div>
-      			<div id="connexion2">
-      				Web [localhost] 8080 <input type="submit" name="delete_2" label="Delete"/>
-      			</div>
-      			<div id="new_connexion">
-      				<select name="type">
-      					<option value="SERIAL">Serial Port</option>
-      					<option value="WEB">Web</option>
-      				</select>
-      				<div id="serial">
-      					<select name="serialPort">
-          					<option value="USB-SERIAL CH340 (COM5)">[USB-SERIAL CH340 (COM5)]</option>
-          					<option value="ELMO GMAS (COM7)">[ELMO GMAS (COM7)]</option>
-          					<option value="USB-SERIAL CH340 (COM6)">[USB-SERIAL CH340 (COM6)]</option>
-          					<option value="Lien série sur Bluetooth standard (COM4)">[Lien série sur Bluetooth standard (COM4)]</option>
-          				</select>	
-          				<select name="serialSpeed">
-          					<option value="9600">9600</option>
-          					<option value="19200">19200</option>
-          					<option value="38400">38400</option>
-          					<option value="57600">57600</option>
-          					<option value="115200">115200</option>
-          				</select>
-      				</div>
-      				<div id="web">
-      					<input type="text" name="host"/>
-      					<input type="text" name="port"/>
-      				</div>
-      			</div>
+      			 <table class="table table-responsive table-striped">
+                      <tr>
+                        <th><?php echo _("Type"); ?></th>
+                        <th><?php echo _("Value1"); ?></th>
+                        <th><?php echo _("Value2"); ?></th>
+                        <th><?php echo _(""); ?></th>
+                        <th></th>
+                      </tr>
+                      <tr>
+                		<td>
+                			Serial port
+                		</td>
+                		<td>
+                			USB-SERIAL CH340 (COM5)
+                		</td>
+                		<td>
+                			57600
+                		</td>
+                		<td>
+                			<input type="submit" name="delete_1" label="Delete"/>
+                		</td>
+                	</tr>
+                	<tr>
+                		<td>
+                			Web
+                		</td>
+                		<td>
+                			localhost
+                		</td>
+                		<td>
+                			8080
+                		</td>
+                		<td>
+                			<input type="submit" name="delete_2" label="Delete"/>
+                		</td>
+                	</tr>
+                	<tr>
+                		<td>
+                			<select name="type">
+              					<option value="SERIAL">Serial Port</option>
+              					<option value="WEB">Web</option>
+              				</select>
+                		</td>
+                		<td>
+                			<select name="serialPort">
+              					<option value="USB-SERIAL CH340 (COM5)">[USB-SERIAL CH340 (COM5)]</option>
+              					<option value="ELMO GMAS (COM7)">[ELMO GMAS (COM7)]</option>
+              					<option value="USB-SERIAL CH340 (COM6)">[USB-SERIAL CH340 (COM6)]</option>
+              					<option value="Lien série sur Bluetooth standard (COM4)">[Lien série sur Bluetooth standard (COM4)]</option>
+              				</select>
+              				<input type="text" name="host"/>
+                		</td>
+                		<td>
+                			<select name="serialSpeed">
+              					<option value="9600">9600</option>
+              					<option value="19200">19200</option>
+              					<option value="38400">38400</option>
+              					<option value="57600">57600</option>
+              					<option value="115200">115200</option>
+              				</select>
+              				<input type="text" name="port"/>
+                		</td>
+                		<td>
+                			<input type="submit" name="add" label="Delete"/>
+                		</td>
+                	</tr>
         	</form>
         </div>
       </div>
