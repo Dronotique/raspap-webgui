c91d71ff2fb59408dd27a2fd5869d9dc8992ce9f
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-10-16 00:00:56 +0200

    ajout de la fonction de delete

--- includes/configurator.php
+++ includes/configurator.php
@@ -1,13 +1,57 @@
 <?php 
 function DisplayConnexionConfig() {
+    $xmlConfFile = simplexml_load_file("configuration.xml");
+    $availableCnx = $xmlConfFile->xpath('/configurator/cameras');
+    
+    $currentConnexions = array ();
+    $i=0;
+    foreach ($availableCnx->children() as $cnx) {
+        if($cnx->xpath('type') == "MAVLINK_SERIAL"){
+            $currentConnexions[$i] = array ("type" => $cnx->xpath('type'), "port" => $cnx->xpath('attributes/serial_port_com'), "speed" => $cnx->xpath('attributes/serial_speed_com'));
+        }else if($cnx->xpath('type') == "MAVLINK_UDP"){
+            $currentConnexions[$i] = array ("type" => $cnx->xpath('type'), "host" => $cnx->xpath('attributes/host'), "port" => $cnx->xpath('attributes/port'));
+        }else{
+            $currentConnexions[$i] = $cnx->children();
+        }
+        $i++;
+    }
+    
+    if(isset($_POST["delete"])){
+        $cameraList = $xmlConfFile->xpath('/configurator/cameras');
+        $serialList = $xmlConfFile->xpath('/configurator/availableSerial');
+       //On réécrit le fichier 
+        $newXmlConf = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><configurator/>");
+        $root = $newXmlConf->xpath('/configurator');
+        $root.addChild("cameras").addChild($cameraList);
+        $root.addChild("availableSerial").addChild($serialList);
+        $connexions = $root.addChild("connexions");
+        
+        for($i=0; $i < sizeof($currentConnexions);$i++){
+            
+            if($_POST["post"]!=$i){
+                $newCnx = $connexions->addChild("connexions");
+                $newCnx->addChild("type", $currentConnexions[$i]["type"]);
+                if($currentConnexions[$i]["type"] == "MAVLINK_SERIAL"){
+                    $attrs = $newCnx->addChild("attributes", "", "xsi:type=\"connexionConfAttrSerial\"");
+                    $attrs->addChild("serial_port_com",  $currentConnexions[$i]["port"]);
+                    $attrs->addChild("serial_speed_com",  $currentConnexions[$i]["speed"]);
+                }else if($currentConnexions[$i]["type"] == "MAVLINK_UDP"){
+                    $attrs = $newCnx->addChild("attributes", "", "xsi:type=\"connexionConfAttrWeb\"");
+                    $attrs->addChild("host",  $currentConnexions[$i]["host"]);
+                    $attrs->addChild("port",  $currentConnexions[$i]["port"]);
+                }
+            }
+        }
+        
+        $newXmlConf->asXml(configuration.xml);
+    }
 ?>
 <div class="row">
     <div class="col-lg-12">
       <div class="panel panel-primary">
-        <div class="panel-heading"><i class="fa fa-lock fa-fw"></i><?php echo _("Configure COnnexions"); ?></div>
+        <div class="panel-heading"><i class="fa fa-lock fa-fw"></i><?php echo _("Configure Connexions"); ?></div>
         <div class="panel-body" id="xonomy_editor">
-      		<form method="POST" action="?page=configurator" name="conf_form">
-      			 <table class="table table-responsive table-striped">
+      			<table class="table table-responsive table-striped">
                       <tr>
                         <th><?php echo _("Type"); ?></th>
                         <th><?php echo _("Value1"); ?></th>
@@ -26,7 +70,10 @@ function DisplayConnexionConfig() {
                 			57600
                 		</td>
                 		<td>
-                			<input type="submit" name="delete_1" label="Delete" class="col-md-6 btn btn-warning"/>
+                			<form method="POST" action="?page=configurator" name="conf_form1">
+      							<input type="hidden" value"1" name="post"/>
+                				<input type="submit" name="delete" label="Delete" class="col-md-6 btn btn-warning"/>
+                			</form>
                 		</td>
                 	</tr>
                 	<tr>
@@ -40,39 +87,44 @@ function DisplayConnexionConfig() {
                 			8080
                 		</td>
                 		<td>
-                			<input type="submit" name="delete_2" label="Delete" class="col-md-6 btn btn-warning"/>
+                			<form method="POST" action="?page=configurator" name="conf_form2">
+      							<input type="hidden" value"2" name="post"/>
+                				<input type="submit" name="delete" label="Delete" class="col-md-6 btn btn-warning"/>
+                			</form>
                 		</td>
                 	</tr>
                 	<tr>
-                		<td>
-                			<select name="type"  class="form-control">
-              					<option value="SERIAL">Serial Port</option>
-              					<option value="WEB">Web</option>
-              				</select>
-                		</td>
-                		<td>
-                			<select name="serialPort" id="serialPort"  class="form-control">
-              					<option value="USB-SERIAL CH340 (COM5)">[USB-SERIAL CH340 (COM5)]</option>
-              					<option value="ELMO GMAS (COM7)">[ELMO GMAS (COM7)]</option>
-              					<option value="USB-SERIAL CH340 (COM6)">[USB-SERIAL CH340 (COM6)]</option>
-              					<option value="Lien série sur Bluetooth standard (COM4)">[Lien série sur Bluetooth standard (COM4)]</option>
-              				</select>
-              				<input type="text" name="host" id="host"  class="form-control" />
-                		</td>
-                		<td>
-                			<select name="serialSpeed" id="serialSpeed"  class="form-control">
-              					<option value="9600">9600</option>
-              					<option value="19200">19200</option>
-              					<option value="38400">38400</option>
-              					<option value="57600">57600</option>
-              					<option value="115200">115200</option>
-              				</select>
-              				<input type="text" name="hostPort" id="hostPort"  class="form-control" />
-                		</td>
-                		<td>
-                			<input type="submit" name="add" label="Delete" class="col-md-6 btn btn-info"/>
-                		</td>
-                	</tr>
+                		<form method="POST" action="?page=configurator" name="conf_form_add">
+                    		<td>
+                    			<select name="type"  class="form-control">
+                  					<option value="SERIAL">Serial Port</option>
+                  					<option value="WEB">Web</option>
+                  				</select>
+                    		</td>
+                    		<td>
+                    			<select name="serialPort" id="serialPort"  class="form-control">
+                  					<option value="USB-SERIAL CH340 (COM5)">[USB-SERIAL CH340 (COM5)]</option>
+                  					<option value="ELMO GMAS (COM7)">[ELMO GMAS (COM7)]</option>
+                  					<option value="USB-SERIAL CH340 (COM6)">[USB-SERIAL CH340 (COM6)]</option>
+                  					<option value="Lien série sur Bluetooth standard (COM4)">[Lien série sur Bluetooth standard (COM4)]</option>
+                  				</select>
+                  				<input type="text" name="host" id="host"  class="form-control" />
+                    		</td>
+                    		<td>
+                    			<select name="serialSpeed" id="serialSpeed"  class="form-control">
+                  					<option value="9600">9600</option>
+                  					<option value="19200">19200</option>
+                  					<option value="38400">38400</option>
+                  					<option value="57600">57600</option>
+                  					<option value="115200">115200</option>
+                  				</select>
+                  				<input type="text" name="hostPort" id="hostPort"  class="form-control" />
+                    		</td>
+                    		<td>
+                    			<input type="submit" name="add" label="Add" class="col-md-6 btn btn-info"/>
+                    		</td>
+                    	</form>
+                  	</tr>
         	</form>
         </div>
       </div>
