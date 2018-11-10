90f2d9933a33a8f654f32d51a8961f213a12cddc
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-10 17:02:23 +0100

    d

--- proxy.php
+++ proxy.php
@@ -86,9 +86,9 @@ if (isset($_REQUEST['csurl'])) {
 } elseif (isset($_SERVER['HTTP_X_PROXY_URL'])) {
     $request_url = urldecode($_SERVER['HTTP_X_PROXY_URL']);
 } else {
-    //header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    //header('Status: 404 Not Found');
-    //$_SERVER['REDIRECT_STATUS'] = 404;
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+    header('Status: 404 Not Found');
+    $_SERVER['REDIRECT_STATUS'] = 404;
     exit;
 }
 $p_request_url = parse_url($request_url);
