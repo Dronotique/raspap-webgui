a946f304613c01d004c8536b3536fd54ab405d31
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-10 16:52:01 +0100

    s

--- includes/configurator_json.php
+++ includes/configurator_json.php
@@ -1,7 +1,7 @@
 <script src="../vendor/jquery/jquery.min.js"></script>
 <script>
-var confAPI = "/includes/proxy.php";
-var confJsonPath = "http://" + window.location.hostname + ":8079/?callback=?";
+var confAPI = "/includes/proxy.php?callback=?";
+var confJsonPath = "http://" + window.location.hostname + ":8079/";
 var confJson = $.getJSON( confAPI, {csurl: confJsonPath},function() {
   console.log( "success" );
 	})
