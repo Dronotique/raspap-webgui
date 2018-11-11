badae8643a335b4c00f9ae1227bacd0d3b3618b2
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-11 18:50:23 +0100

    ddd

--- includes/configurator_json.php
+++ includes/configurator_json.php
@@ -170,13 +170,13 @@ function DisplayConnexionConfigJson() {
 		
 		if(liveviewType == "HTTP_MJPEG"){ 
 			var httpPort = $("#newLiveviewPort").val();
-			liveview = {"type":liveviewType,"attributes":{"class":"LiveviewConfAttrHttp","port":httpPort},"filters":{}}
+			liveview = {"type":liveviewType,"attributes":{"class":"LiveviewConfAttrHttp","port":httpPort},"filters":{}};
 		}else{
-			liveview = {"type":liveviewType,"attributes":{"class":"LiveviewConfAttr"},"filters":{}}
+			liveview = {"type":liveviewType,"attributes":{"class":"LiveviewConfAttr"},"filters":{}};
 		}
 
 		$("#sortableSelectedFilters").each(function( index ) {
-			liveview.filters.push({"type" : $( this ).text(), "parameters" : null});
+			liveview["filters"].push({"type" : $( this ).text(), "parameters" : null});
 		});
 		confJson.cameras[camera].liveviews.push(liveview);
 
