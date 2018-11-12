826ee30f9a235114b6f9c8300f39aabdf653e416
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-12 10:59:26 +0100

    ggg

--- proxy.php
+++ proxy.php
@@ -55,12 +55,13 @@ $curl_options = array(
 // identify request headers
 $request_headers = array( );
 foreach ($_SERVER as $key => $value) {
-    csajax_debug_message("$key\n");
+    
     if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
         $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
         $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
         if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
             $request_headers[] = "$headername: $value";
+            csajax_debug_message("$headername\n");
         }
     }
 }
