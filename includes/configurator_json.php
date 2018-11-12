034d3aac95eb6b1eaa66af393572a10e10c50a6b
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-12 01:40:54 +0100

    l

--- includes/configurator_json.php
+++ includes/configurator_json.php
@@ -354,10 +354,10 @@ function DisplayConnexionConfigJson() {
 		$.ajax({
 			  traditional: true,
 			  type: "POST",
-			  url: confAPI + "?csurl=" + confJsonPath,
+			  url: confAPI,
 			  dataType: 'json',
 	          contentType: 'application/json',
-			  data : {action: 'update', jsonConf : JSON.stringify(confJson)},
+			  data : {csurl: confJsonPath, action: 'update', jsonConf : JSON.stringify(confJson)},
 			  success: function(newData){
 				  confJson = newData;
 				  refreshTableCamera();
