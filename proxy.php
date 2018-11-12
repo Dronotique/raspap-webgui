2263b7fb85237d73ca58c353183a37c140424cc6
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-12 10:51:55 +0100

    ,,,,

--- proxy.php
+++ proxy.php
@@ -55,6 +55,8 @@ $curl_options = array(
 // identify request headers
 $request_headers = array( );
 foreach ($_SERVER as $key => $value) {
+    csajax_debug_message($key . ":" . $value ."\n");
+    
     if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
         $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
         $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
