f003632fc9315dd6117c2097436a8f92299b9190
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-12 10:56:21 +0100

    kkk

--- proxy.php
+++ proxy.php
@@ -58,9 +58,9 @@ foreach ($_SERVER as $key => $value) {
     if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
         $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
         $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
+        csajax_debug_message("$headername: $value\n");
         if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
             $request_headers[] = "$headername: $value";
-            csajax_debug_message("$headername: $value\n");
         }
     }
 }
