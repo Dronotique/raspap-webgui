3ec43e2927171bf643020f1981d0bbe525833aa3
Dronotique <39765595+Dronotique@users.noreply.github.com>
2018-11-19 01:48:49 +0100

    jjjj

--- includes/openvpn.php
+++ includes/openvpn.php
@@ -47,10 +47,10 @@ function DisplayOpenVPNConfig() {
 	exec( 'pidof openvpn | wc -l', $openvpnstatus);
 	
 	if( $openvpnstatus[0] == 0 ) {
-		$status = '<div class="alert alert-warning alert-dismissable">OpenVPN is not running
+		$statusMsg = '<div class="alert alert-warning alert-dismissable">OpenVPN is not running
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>';
 	} else {
-		$status = '<div class="alert alert-success alert-dismissable">OpenVPN is running
+	    $statusMsg = '<div class="alert alert-success alert-dismissable">OpenVPN is running
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>';
 	}
 
@@ -76,7 +76,7 @@ function DisplayOpenVPNConfig() {
 			<div class="panel-heading"><i class="fa fa-lock fa-fw"></i> Configure OpenVPN </div>
 		<!-- /.panel-heading -->
 		<div class="panel-body">
-			<p><?php //$status->showMessages(); ?></p>
+			<p><?php $status->showMessages(); ?></p>
 			<!-- Nav tabs -->
 			<!-- ul class="nav nav-tabs">
 				<li class="active"><a href="#openvpnclient" data-toggle="tab">Client settings</a></li>
@@ -86,7 +86,7 @@ function DisplayOpenVPNConfig() {
 			<div class="tab-content">
 				<form role="form" action="?page=openvpn_conf" method="POST">
 				<?php CSRFToken() ?>
-				<p><?php echo $status; ?></p>
+				<p><?php echo $statusMsg; ?></p>
 				<div class="tab-pane fade in active" id="openvpnclient">
 
 					<h4>Client settings</h4>
@@ -127,43 +127,43 @@ function DisplayOpenVPNConfig() {
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">Port</label> 
-						<input type="text" class="form-control" name="openvpn_port" value="<?php echo htmlspecialchars($arrServerConfig['port'], ENT_QUOTES); ?>" />
+						<input type="text" class="form-control" name="openvpn_port" value="<?php //echo htmlspecialchars($arrServerConfig['port'], ENT_QUOTES); ?>" />
 						</div>
 					</div>
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">Protocol</label>
-						<input type="text" class="form-control" name="openvpn_proto" value="<?php echo htmlspecialchars($arrServerConfig['proto'], ENT_QUOTES); ?>" />
+						<input type="text" class="form-control" name="openvpn_proto" value="<?php //echo htmlspecialchars($arrServerConfig['proto'], ENT_QUOTES); ?>" />
 						</div>
 					</div>
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">Root CA certificate</label>
-						<input type="text" class="form-control" name="openvpn_rootca" placeholder="<?php echo htmlspecialchars($arrServerConfig['ca'], ENT_QUOTES); ?>" disabled="disabled" />
+						<input type="text" class="form-control" name="openvpn_rootca" placeholder="<?php //echo htmlspecialchars($arrServerConfig['ca'], ENT_QUOTES); ?>" disabled="disabled" />
 						</div>
 					</div>
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">Server certificate</label>
-						<input type="text" class="form-control" name="openvpn_cert" placeholder="<?php echo htmlspecialchars($arrServerConfig['cert'], ENT_QUOTES); ?>" disabled="disabled" />
+						<input type="text" class="form-control" name="openvpn_cert" placeholder="<?php //echo htmlspecialchars($arrServerConfig['cert'], ENT_QUOTES); ?>" disabled="disabled" />
 						</div>
 					</div>
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">Diffie Hellman parameters</label>
-						<input type="text" class="form-control" name="openvpn_dh" placeholder="<?php echo htmlspecialchars($arrServerConfig['dh'], ENT_QUOTES); ?>" disabled="disabled" />
+						<input type="text" class="form-control" name="openvpn_dh" placeholder="<?php //echo htmlspecialchars($arrServerConfig['dh'], ENT_QUOTES); ?>" disabled="disabled" />
 						</div>
 					</div>
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">KeepAlive</label>
-						<input type="text" class="form-control" name="openvpn_keepalive" value="<?php echo htmlspecialchars($arrServerConfig['keepalive'], ENT_QUOTES); ?>" />
+						<input type="text" class="form-control" name="openvpn_keepalive" value="<?php //echo htmlspecialchars($arrServerConfig['keepalive'], ENT_QUOTES); ?>" />
 						</div>
 					</div>
 					<div class="row">
 						<div class="form-group col-md-4">
 						<label for="code">Server log</label>
-						<input type="text" class="form-control" name="openvpn_status" placeholder="<?php echo htmlspecialchars($arrServerConfig['status'], ENT_QUOTES); ?>" disabled="disabled" />
+						<input type="text" class="form-control" name="openvpn_status" placeholder="<?php //echo htmlspecialchars($arrServerConfig['status'], ENT_QUOTES); ?>" disabled="disabled" />
 						</div>
 					</div>
 				</div-->
